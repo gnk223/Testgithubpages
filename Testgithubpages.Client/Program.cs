a4abbcf69c6aad98631bffcf51d8ef0c1d72ba67
nagesh <gnageshkumar8@gmail.com>
2025-07-06 01:00:02 +0530

    config pages

--- Testgithubpages.Client/Program.cs
+++ Testgithubpages.Client/Program.cs
@@ -1,5 +1,14 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Web;
+using Testgithubpages;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Configure the base address for HttpClient
+var baseAddress = builder.HostEnvironment.BaseAddress;
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
 
 await builder.Build().RunAsync();
